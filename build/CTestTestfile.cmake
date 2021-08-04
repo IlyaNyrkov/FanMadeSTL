5cd8fa7fd81f3bb090f0cd7cf244bf70dddee6d3
Ilya Nyrkov <71572784+IlyaNyrkov@users.noreply.github.com>
2021-08-04 18:53:11 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilya/petProjects/CppProjects/DataStructures
-# Build directory: /home/ilya/petProjects/CppProjects/DataStructures/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
